4d36de2f46029e9a459a6c9f0cbaabcac6c90865
Colin Nico Schmid <mail@mala23.me>
2017-11-18 14:58:24 +0100

    break out snake object

--- game.js
+++ game.js
@@ -22,25 +22,3 @@ function keyPressed() {
     snake.dir(1, 0);
   }
 }
-
-function Snake() {
-  this.x = 0;
-  this.y = 0;
-  this.xSpeed = 1;
-  this.ySpeed = 0;
-
-  this.update = function() {
-    this.x = this.x + this.xSpeed;
-    this.y = this.y + this.ySpeed;
-  }
-
-  this.show = function() {
-    fill(255);
-    rect(this.x, this.y, 10, 10);
-  }
-
-  this.dir = function(x, y) {
-    this.xSpeed = x;
-    this.ySpeed = y;
-  }
-}
---
+++ snake.js
@@ -0,0 +1,21 @@
+function Snake() {
+  this.x = 0;
+  this.y = 0;
+  this.xSpeed = 1;
+  this.ySpeed = 0;
+
+  this.update = function() {
+    this.x = this.x + this.xSpeed;
+    this.y = this.y + this.ySpeed;
+  }
+
+  this.show = function() {
+    fill(255);
+    rect(this.x, this.y, 10, 10);
+  }
+
+  this.dir = function(x, y) {
+    this.xSpeed = x;
+    this.ySpeed = y;
+  }
+}
