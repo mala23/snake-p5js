58686882d9a86c13791e3348a30b26c4c87ed84a
Colin Nico Schmid <mail@mala23.me>
2017-11-18 15:46:33 +0100

    create food

--- game.js
+++ game.js
@@ -5,12 +5,16 @@ function setup() {
   createCanvas(600, 600);
   snake = new Snake();
   frameRate(10);
+  food = createVector(random(width), random(height));
 }
 
 function draw() {
   background(51);
   snake.update();
   snake.show();
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, gameScale, gameScale);
 }
 
 function keyPressed() {
