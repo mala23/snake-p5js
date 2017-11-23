84ba8581491592ff12ea333aee5e0ce1db307981
Colin Nico Schmid <mail@mala23.me>
2017-11-23 13:59:53 +0100

    fix snake eat

--- snake.js
+++ snake.js
@@ -3,8 +3,27 @@ function Snake() {
   this.y = 0;
   this.xSpeed = 1;
   this.ySpeed = 0;
+  this.total = 0;
+  this.tail = [];
+
+  this.eat = function(position) {
+    var d = dist(this.x, this.y, position.x, position.y);
+    if (d < 1) {
+      this.total++;
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   this.update = function() {
+    if (this.total === this.tail.length) {
+      for (var i = 0; i < this.tail.length - 1; i++) {
+        this.tail[i] = this.tail[i + 1];
+      }
+    }
+    this.tail[this.total - 1] = createVector(this.x, this.y);
+
     this.x = this.x + this.xSpeed * gameScale;
     this.y = this.y + this.ySpeed * gameScale;
 
@@ -14,20 +33,14 @@ function Snake() {
 
   this.show = function() {
     fill(255);
+    for (var i = 0; i < this.total; i++) {
+      rect(this.tail[i].x, this.tail[i].y, gameScale, gameScale);
+    }
     rect(this.x, this.y, gameScale, gameScale);
   }
 
   this.dir = function(x, y) {
     this.xSpeed = x;
     this.ySpeed = y;
-  }
-
-  this.eat = function(position) {
-    var d = dist(this.x, this.y, position.x, position.y);
-    if (d < 1) {
-      return true;
-    } else {
-      return false;
-    }
   }
 }
